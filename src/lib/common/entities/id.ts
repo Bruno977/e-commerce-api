aae9553eaaf81e1840e1347bbccfdf5ff9e8eeb8
Bruno Rodrigues <bruno.silveira@hsl.org.br>
2025-05-09 16:53:46 -0300

    chore: remove comments

--- src/lib/common/entities/id.ts
+++ src/lib/common/entities/id.ts
@@ -4,37 +4,16 @@ export class Id {
       throw new Error('Id cannot be empty');
     }
   }
+  static create(id?: string): Id {
+    return new Id(id ?? crypto.randomUUID());
+  }
   toValue(): string {
+    return this._value;
+  }
+  toString(): string {
     return this._value;
   }
   equals(id: Id): boolean {
     return this._value === id._value;
   }
-  toString(): string {
-    return this._value;
-  }
-  static create(id?: string): Id {
-    return new Id(id ?? crypto.randomUUID());
-  }
 }
-// import { randomUUID } from 'node:crypto';
-
-// export class UniqueEntityID {
-//   private value: string;
-
-//   toString() {
-//     return this.value;
-//   }
-
-//   toValue() {
-//     return this.value;
-//   }
-
-//   constructor(value?: string) {
-//     this.value = value ?? randomUUID();
-//   }
-
-//   public equals(id: UniqueEntityID) {
-//     return id.toValue() === this.value;
-//   }
-// }
