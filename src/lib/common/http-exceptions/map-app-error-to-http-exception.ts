de26147456740442cce8014dce916b01c4b56811
Bruno Rodrigues <bruno.silveira@hsl.org.br>
2025-06-23 16:54:37 -0300

    chore: add doc find by id category

--- src/lib/common/http-exceptions/map-app-error-to-http-exception.ts
+++ src/lib/common/http-exceptions/map-app-error-to-http-exception.ts
@@ -1,11 +1,13 @@
 import {
   BadRequestException,
   ConflictException,
+  NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
 import { AppError } from '../errors/app-error';
 import { NotAllowedError } from '../errors/not-allowed.error';
 import { ResourceAlreadyExistsError } from '../errors/resource-already-exists.error';
+import { ResourceNotFoundError } from '../errors/resource-not-found.error';
 
 export function mapAppErrorToHttpException(error: AppError) {
   if (error instanceof NotAllowedError) {
@@ -14,6 +16,9 @@ export function mapAppErrorToHttpException(error: AppError) {
 
   if (error instanceof ResourceAlreadyExistsError) {
     return new ConflictException(error.message);
+  }
+  if (error instanceof ResourceNotFoundError) {
+    return new NotFoundException(error.message);
   }
 
   return new BadRequestException(error.message);
--- src/modules/category/infra/http/controllers/find-category-by-id.controller.ts
+++ src/modules/category/infra/http/controllers/find-category-by-id.controller.ts
@@ -3,14 +3,24 @@ import { mapAppErrorToHttpException } from 'src/lib/common/http-exceptions/map-a
 import { Public } from 'src/modules/auth/infra/auth/public';
 import { FindCategoryByIdUseCase } from 'src/modules/category/application/use-cases/find-category-by-id.use-case';
 import { CategoryPresenter } from '../presenters/category.presenter';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('/categories/:id')
 @Public()
+@ApiTags('Category')
 export class FindCategoryByIdController {
   constructor(private findCategoryById: FindCategoryByIdUseCase) {}
 
   @Get()
   @HttpCode(200)
+  @ApiOperation({
+    summary: 'Find a category by ID',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Category found successfully',
+  })
+  @ApiResponse({ status: 404, description: 'Category not found' })
   async handle(@Param('id') id: string) {
     const result = await this.findCategoryById.execute(id);
     if (result.isLeft()) {
